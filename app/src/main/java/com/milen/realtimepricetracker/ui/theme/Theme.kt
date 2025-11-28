f77d0629e826c65333bb6973994247160855fd40
Milen Dimitrov <milen.dimitrov@paysafe.com>
2025-11-28 20:04:33 +0200

    feat(theme): Set up the theme

--- app/src/main/java/com/milen/realtimepricetracker/ui/theme/Theme.kt
+++ app/src/main/java/com/milen/realtimepricetracker/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.milen.realtimepricetracker.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,54 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BluePrimaryDark,
+    onPrimary = Color.White,
+    primaryContainer = BluePrimary,
+    onPrimaryContainer = Color.White,
+
+    secondary = GreenAccentDark,
+    onSecondary = Color.White,
+    secondaryContainer = GreenAccent,
+    onSecondaryContainer = Color.White,
+
+    tertiary = TealTertiary,
+    onTertiary = Color.White,
+    tertiaryContainer = TealTertiaryLight,
+    onTertiaryContainer = Color.White,
+
+    background = LogoSurfaceDark,
+    onBackground = Color.White,
+    surface = LogoSurfaceDark,
+    onSurface = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = BluePrimary,
     onPrimary = Color.White,
+
+    primaryContainer = BluePrimaryLight,
+    onPrimaryContainer = Color(0xFF002747),
+
+    secondary = GreenAccent,
     onSecondary = Color.White,
+
+    secondaryContainer = GreenAccentLight,
+    onSecondaryContainer = Color(0xFF003827),
+
+    tertiary = TealTertiary,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+    tertiaryContainer = TealTertiaryLight,
+    onTertiaryContainer = Color(0xFF002022),
+
+    background = LogoSurfaceLight,
+    onBackground = Color(0xFF001322),
+    surface = Color.White,
+    onSurface = Color(0xFF001322)
 )
 
 @Composable
@@ -38,7 +63,7 @@ fun RealTimePriceTrackerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
